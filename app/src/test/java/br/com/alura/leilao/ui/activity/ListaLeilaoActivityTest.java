3dd6ae8f13c72709777bd2b1d1a3e232fb666087
Paulo Iury <paulo.rocha@gfxconsultoria.com>
2021-01-14 11:20:57 -0300

    Manipulando teste para não depender dá chamada da API

--- app/src/test/java/br/com/alura/leilao/ui/activity/ListaLeilaoActivityTest.java
+++ app/src/test/java/br/com/alura/leilao/ui/activity/ListaLeilaoActivityTest.java
@@ -1,15 +1,63 @@
 package br.com.alura.leilao.ui.activity;
 
+import android.content.Context;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.api.retrofit.client.RespostaListener;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ListaLeilaoActivityTest {
+    @Mock
+    private Context context;
+
+    @Spy
+    private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
+
+    @Mock
+    private LeilaoWebClient client;
+
+
     @Test
-    public void deve_AtualizarListaDeLeiloes_QuandoBuscarLeiloesDaApi(){
+    public void deve_AtualizarListaDeLeiloes_QuandoBuscarLeiloesDaApi() {
         ListaLeilaoActivity activity = new ListaLeilaoActivity();
-        activity.onResume();
-        int quantidadeLeiloesDevolvida = activity.getAdapter().getItemCount();
+        Mockito.doNothing().when(adapter).atualizaLista();
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                RespostaListener<List<Leilao>> argument = invocation.getArgument(0);
+                argument.sucesso(new ArrayList<>(Arrays.asList(
+                        new Leilao("Computador"),
+                        new Leilao("Console"),
+                        new Leilao("Monaliza")
+                )));
+                return null;
+            }
+        }).when(client).todos(ArgumentMatchers.any(RespostaListener.class));
 
+        activity.buscaLeiloes(adapter, client);
 
+        int quantidadeLeiloesDevolvida = adapter.getItemCount();
+
+        assertThat(quantidadeLeiloesDevolvida,is(3));
     }
 
 }
